28f756251701a190b8fe992602840e6acac478a7
sc6l6d3v <hk-vndr@iscs-i.com>
2022-11-30 15:44:13 -0500

    Fix Uri test.

--- src/test/scala/com/iscs/tmdbimg/api/TMDBImgApiUriSpec.scala
+++ src/test/scala/com/iscs/tmdbimg/api/TMDBImgApiUriSpec.scala
@@ -5,26 +5,29 @@ import org.specs2.mutable.Specification
 
 class TMDBImgApiUriSpec extends Specification {
 
-  private val base = "https://api.ipgeolocation.io"
-  private val path = "/ipgeo"
-  private val keyParam = "apiKey"
-  private val keyValue = sys.env.getOrElse("GEOIPKEY", "NOKEY")
-  private val fullBase = s"$base$path?$keyParam=$keyValue"
-  private val fieldsParam = "fields"
-  private val fieldsValues = "geo,organization"
+  private val base = "https://api.themoviedb.org"
+  private val path = "/3/find/"
+  private val keyParam = "api_key"
+  private val keyValue = sys.env.getOrElse("TMDBKEY", "NOKEY")
+  private def fullBase(maybeKey: Option[String]) = maybeKey match {
+    case Some(key) => s"$base$path$key?$keyParam=$keyValue"
+    case _         => s"$base$path?$keyParam=$keyValue"
+  }
+  private val fieldMap = Map("language" -> "en-US", "external_source" -> "imdb_id")
+  private val fields = fieldMap.map{ case (k,v) => s"$k=$v"}.mkString("&")
 
   private[this] def baseCheck(): MatchResult[String] =
-    GeoIPApiUri.builder(GeoIPApiUri("")) must beEqualTo(s"$fullBase&ip=&$fieldsParam=$fieldsValues")
+    TMDBApiUri.builder(TMDBApiUri(FIND, "")) must beEqualTo(s"${fullBase(Some(""))}&$fields")
 
-  private[this] def withIPCheck(): MatchResult[String] =
-    GeoIPApiUri.builder(GeoIPApiUri("211.40.129.246")) must beEqualTo(s"$fullBase&ip=211.40.129.246&$fieldsParam=$fieldsValues")
+  private[this] def withIMDBId(): MatchResult[String] =
+    TMDBApiUri.builder(TMDBApiUri(FIND, "tt104343")) must beEqualTo(s"${fullBase(Some("tt104343"))}&$fields")
 
-  "GeoIPApiUri" >> {
+  "TMDBApiUri" >> {
     "base checks" >> {
       baseCheck()
     }
     "withQuery override" >> {
-      withIPCheck()
+      withIMDBId()
     }
   }
 
